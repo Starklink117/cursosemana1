0a6f5e978fcb0501acbcc45dd8600703744cfd1e
Starklink117 <luisangel8227@gmail.com>
2023-08-21 12:55:40 -0600

    Ultimo cambio 21-08-23

--- resources/views/estudiante/index.blade.php
+++ resources/views/estudiante/index.blade.php
@@ -3,40 +3,78 @@
 @section('titulo', 'Ver Estudiantes')
 
 @section('contenido')
+
+@section('css')
+<link href="https://cdn.datatables.net/1.13.1/css/dataTables.bootstrap5.min.css" rel="stylesheet">
+@endsection
+
+
     <h1 class="my-3">INDEX de Estudiantes</h1>
     <br>
     <a href="estudiantes/create" class="btn btn-primary">Crear</a>
-    <table class="table mt-3">
-        <thead class="table-dark">
-            <tr>
-            <th class="text-light" scope="col">ID</th>
-            <th class="text-light" scope="col">MATRICULA</th>
-            <th class="text-light" scope="col">NOMBRE</th>
-            <th class="text-light" scope="col">APELLIDO PATERNO</th>
-            <th class="text-light" scope="col">APELLIDO MATERNO</th>
-            <th class="text-light" scope="col">CORREO</th>
-            <th class="text-light" scope="col">ACCIONES</th>
-            </tr>
-        </thead>
-        <tbody>
-            @foreach ($estudiantes as $estudiante)
+    <div class="table-responsive my-3">
+        <table id="estudiantet" class="table mt-3">
+            <thead class="table-dark">
                 <tr>
-                    <td>{{$estudiante->id}}</td>
-                    <td>{{$estudiante->matricula}}</td>
-                    <td>{{$estudiante->nombre}}</td>
-                    <td>{{$estudiante->apellidopaterno}}</td>
-                    <td>{{$estudiante->apellidomaterno}}</td>
-                    <td>{{$estudiante->correo}}</td>
-                    <td>
-                        <form action="{{route ('estudiantes.destroy',$estudiante->id)}}" method="post">
-                            @csrf
-                            @method('DELETE')
-                            <a href="/estudiantes/{{$estudiante->id}}/edit" class="btn btn-warning">Editar</a>
-                            <button type="submit" class="btn btn-danger">Eliminar</button>
-                        </form>
-                    </td>
+                <th class="text-light" scope="col">ID</th>
+                <th class="text-light" scope="col">MATRICULA</th>
+                <th class="text-light" scope="col">NOMBRE</th>
+                <th class="text-light" scope="col">APELLIDO PATERNO</th>
+                <th class="text-light" scope="col">APELLIDO MATERNO</th>
+                <th class="text-light" scope="col">CORREO</th>
+                <th class="text-light" scope="col">ACCIONES</th>
                 </tr>
-            @endforeach
-        </tbody>
-    </table>
+            </thead>
+            <tbody>
+                @foreach ($estudiantes as $estudiante)
+                    <tr>
+                        <td>{{$estudiante->id}}</td>
+                        <td>{{$estudiante->matricula}}</td>
+                        <td>{{$estudiante->nombre}}</td>
+                        <td>{{$estudiante->apellidopaterno}}</td>
+                        <td>{{$estudiante->apellidomaterno}}</td>
+                        <td>{{$estudiante->correo}}</td>
+                        <td>
+                            <a href="/estudiantes/{{$estudiante->id}}/edit" class="btn btn-warning">Editar</a>
+                            <button type="button" class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#modal-delete-{{$estudiante->id}}">
+                                Eliminar
+                            </button>
+                        </td>
+                    </tr>
+                    @include('estudiante.modal')
+                @endforeach
+            </tbody>
+        </table>
+    </div>
+    
+
+    @section('js')
+    <script src="https://code.jquery.com/jquery-3.5.1.js"></script> {{-- Es para lo de datatable --}}
+    <script src="https://cdn.datatables.net/1.13.1/js/jquery.dataTables.min.js"></script> {{-- Es para lo de datatable --}}
+    <script src="https://cdn.datatables.net/1.13.1/js/dataTables.bootstrap5.min.js"></script> {{-- Es para lo de datatable --}}
+    {{-- Es para lo de datatable y se pone el id de nuestra tabla --}}
+    <script>
+        $(document).ready(function () {
+            $('#estudiantet').DataTable({
+                language: {
+                "lengthMenu": "Mostrar _MENU_ Registros",
+                "zeroRecords": "No se encontraron resultados",
+                "info": "Mostrando registros del _START_ al _END_ de un total de _TOTAL_ registros",
+                "infoEmpty": "Mostrando registros del 0 al 0 de un total de 0 registros",
+                "infoFiltered": "(filtrado de un total de _MAX_ registros)",
+                "sSearch": "Buscar:",
+                "oPaginate": {
+                    "sFirst": "Primero",
+                    "sLast":"Último",
+                    "sNext":"Siguiente",
+                    "sPrevious": "Anterior"
+			},
+            "sProcessing":"Procesando...",
+                },
+                responsive: "true",
+            });
+        });
+    </script>
+    @endsection
+
 @endsection
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,6 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>@yield('titulo')</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+    @yield('css')
 </head>
 <body>
 
@@ -17,5 +18,6 @@
         @yield('contenido')
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
+    @yield('js')
 </body>
 </html>
